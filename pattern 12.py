e4fb7f15c15b784355edb10c772cd19f4d663998
boyamounika9 <189mounika.b@gmail.com>
2026-03-10 20:18:19 +0530

    changes

--- pattern 12.py
+++ pattern 12.py
@@ -1,7 +1,7 @@
 n=int(input())
-for i in range (n):
-    for j in range (n):
-        if i+j==n-1:
+for i in range (1,n+1):
+    for j in range (1,n+1):
+        if i+j==n+1:
             print(1,end=" ")
         else:
             print(0,end=" ")
