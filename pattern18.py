3f38481998a668d1c5e261feb35c0913e33baac4
boyamounika9 <189mounika.b@gmail.com>
2026-03-11 16:50:54 +0530

    adding

--- pattern18.py
+++ pattern18.py
@@ -1,14 +1,9 @@
 n=4
 for i in range(1,n+1):
-    for j in range(1,n+1):
-        # if i==4 or j==4:
-        #     print(4,end=" ")
-        # elif i==3 or j==3:
-        #     print(3,end=" ")
-        # elif i==2 or j==2:
-        #     print(2,end=" ")
-        # elif i==1 or j==1:
-        #     print(1,end=" ")
-        
-
+    for j in range(0,n):
+        if j<i:
+            print(i,end=" ")
+        else:
+            print(j,end=" ")
     print()
+
